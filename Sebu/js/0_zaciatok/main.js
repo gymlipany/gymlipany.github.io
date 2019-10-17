4d9f68233187cbae3ffb49a8bf7036a5b257a5ca
gymlipany <37588803+gymlipany@users.noreply.github.com>
2019-10-17 15:23:27 +0200

    xd

---
+++ Sebu/js/0_zaciatok/main.js
@@ -0,0 +1,38 @@
+// kometntar
+
+/*
+viac riadkov
+*/
+
+// vypis ahoj 4 krat
+// let a = "bonifac"
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+// nie -> ahoj_svet
+// nie -> AhojSvet
+// ano -> ahojSvet
+
+// let cislo = 0.5
+// let text = "Ahoj more hento je text."
+// let somDoma = false
+
+// console.log(cislo)
+// console.log(text)
+// console.log(somDoma)
+// let a = 2
+// let b = 5
+// let c = 2*a + 2*b
+// console.log(c)
+
+// let a = 1 + 2
+// let b = 2 - 1
+// let c = 2 * 2
+// let d = 5 / 3
+// let e = 5 % 3 // aky zvysok dostanem ak 5 vydelim 3 celociselne
+// let f = Math.round(5 / 3)
+
+// let okruhlenie = Math.round(5/3)
+
