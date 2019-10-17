44034666d0d15325d527b7040a4977ab76d2328a
gymlipany <37588803+gymlipany@users.noreply.github.com>
2019-10-17 15:52:50 +0200

    zjghjh

---
+++ Cico/js/0_zaciatok/main.js
@@ -0,0 +1,26 @@
+// vypíš Ahoj 4x
+
+let a = "Serus"
+
+console.log(a)
+console.log(a)
+console.log(a)
+console.log(a)
+
+// let cislo = 5.5
+// let text = "Ahoj!"
+// let somDoma = false 
+
+// let a = 4
+// let b = 9
+// let c = a + b 
+
+let b = 5
+let c = 4
+let d = b - c 
+console.log(d)
+
+//vstup 
+let x = parseInt(prompt("Zadajte x:"))
+let y = parseInt(prompt("zadajte y:"))
+alert(x + y)
