1a0027f28a1b4e8d20b1e9624a1d19705768d4b5
gymko lipany <gymlipanygit@gmail.com>
2019-10-17 16:04:06 +0200

    xD

---
+++ DžorDži/notes/main copy.js
@@ -0,0 +1,66 @@
+// kometntar
+
+/*
+viac riadkov
+*/
+
+// vypis ahoj 4 krat
+// let a = "bonifac"
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+// nie -> ahoj_svet
+// nie -> AhojSvet
+// ano -> ahojSvet
+
+// let cislo = 0.5
+// let text = "Ahoj more hento je text."
+// let somDoma = false
+
+// console.log(cislo)
+// console.log(text)
+// console.log(somDoma)
+// let a = 2
+// let b = 5
+// let c = 2*a + 2*b
+// console.log(c)
+
+// let a = 1 + 2
+// let b = 2 - 1
+// let c = 2 * 2
+// let d = 5 / 3
+// let e = 5 % 3 // aky zvysok dostanem ak 5 vydelim 3 celociselne
+// let f = Math.round(5 / 3)
+
+// let okruhlenie = Math.round(5/3)
+
+// let a = "Ahoj"
+// let c = "svet."
+
+// console.log(a + c)
+
+// let cislo = 3
+// let text = "Hento je "
+// let cisloString = String(cislo)
+// console.log(cisloString + 3)
+
+// console.log("3" + "3") // vysledok "33"
+// console.log( 3  +  3 ) // vyledok 6
+
+// console.log(typeof(3))
+
+// vstup
+// let a = parseInt(prompt("Zadajte a:"))
+// let b = parseInt(prompt("Zadajte b:"))
+// alert(a + b)
+
+// vypis do konzoly
+console.log("ahoj")
+
+// vyskakovacie okno
+alert("ahoj")
+
+// pripoj na koniec sttranky dalsi text
+document.write("ahoj")
