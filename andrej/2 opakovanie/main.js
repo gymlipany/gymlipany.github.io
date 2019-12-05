26a03cf12e54172ddb008916ede57897b200c89e
gymlipany <37588803+gymlipany@users.noreply.github.com>
2019-12-05 16:05:43 +0100

    ghghh

---
+++ andrej/2 opakovanie/main.js
@@ -0,0 +1,7 @@
+let a = 3
+let b = 6
+ let vyraz = a < b
+ 
+ if (a < b) {
+     console.log("jee")
+ }
---
+++ andrej/js/0 zaciatok/main.js
@@ -0,0 +1,18 @@
+console.log("andrej")
+console.log("sekerak")
+
+console.log("kaine problem") //dfjd
+
+// kaine problem
+// vypis ahoj 4 krat 
+let a = "ahojs"
+console.log(a)
+console.log(a)
+console.log(a)
+console.log(a)
+let d = "ahoj"
+let b = ""
+let c = "gf"
+console.log(d + b +c) 
+
+document.write
