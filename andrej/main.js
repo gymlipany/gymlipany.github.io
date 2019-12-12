8520c62fa0b8ad4a843a281be5939a4647821327
gymlipany <37588803+gymlipany@users.noreply.github.com>
2019-12-12 15:30:52 +0100

    fb
    fbb

---
+++ andrej/main.js
@@ -0,0 +1,15 @@
+for (let i = 50; i <= 100; i = i + 5) {
+    if (( i != 60) || ( i == 70)) {
+         continue;
+    } 
+   console.log(i) 
+}
+i = 50 
+while (i < 100){
+    i = i + 5
+    if (( i == 60) || ( i == 70)) {
+        break;
+   } 
+    console.log(i)
+}
+
