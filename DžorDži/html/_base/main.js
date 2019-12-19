4fcc138da1911e3eb0e0be81f8310ccd730a00e0
gymko lipany <gymlipanygit@gmail.com>
2019-12-19 20:10:58 +0100

    xd

--- DžorDži/html/_base/main.js
+++ DžorDži/html/_base/main.js
@@ -1,1 +1,12 @@
-
+let b = 0
+let a = 0
+function setup() {
+    createCanvas(1366, 600)
+}
+function draw() {
+    stroke(200, 0, 0)
+    line(2000, 0, 0, b*0.9)
+    a++
+    b += a/0.6
+}
+$("#1").click(setup)
