f0ba3e52c30afd1fceee1d6b7df463809831487b
gymlipany <37588803+gymlipany@users.noreply.github.com>
2020-01-16 16:10:29 +0100

    zadanir

---
+++ Sebu/js/12_zadanie_objekty/main.js
@@ -0,0 +1,18 @@
+/*
+- nakopiruj si novy projekt z base
+- pomenuj ho 12_zadanie_objekty
+- pis priamo do main.js,
+  vymaz funkcie setup a draw (nepotrebujeme ich)
+
+ZADANIE:
+- vytvorte triedu, ktora opisuje psa
+- pes ma meno, vek a hlad (cislo)
+- pes vie brechat - "Haf haf!"
+- pes vie zit - prida sa mu vek o 1 a tiez sa mu prida hlad o 1
+  (je viac hladny lebo uz presiel rok a nic nejedol)
+- pes vie jest
+    - vypise do konzoly ze Ham ham
+    - hlad sa mu odpocita o 1,
+        ALE len v pripade ak je hlad vacsi ako 0 !!!
+
+*/
