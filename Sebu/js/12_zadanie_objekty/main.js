f63d31e4500f188aa60a8f5e1a10d5a08ed3b19c
gymlipany <37588803+gymlipany@users.noreply.github.com>
2020-01-16 16:19:15 +0100

    asd

--- Sebu/js/12_zadanie_objekty/main.js
+++ Sebu/js/12_zadanie_objekty/main.js
@@ -6,13 +6,20 @@
 
 ZADANIE:
 - vytvorte triedu, ktora opisuje psa
-- pes ma meno, vek a hlad (cislo)
-- pes vie brechat - "Haf haf!"
-- pes vie zit - prida sa mu vek o 1 a tiez sa mu prida hlad o 1
-  (je viac hladny lebo uz presiel rok a nic nejedol)
-- pes vie jest
-    - vypise do konzoly ze Ham ham
-    - hlad sa mu odpocita o 1,
-        ALE len v pripade ak je hlad vacsi ako 0 !!!
+    - pes ma meno, vek a hlad (cislo)
+
+    - pes vie brechat - "Haf haf!"
+    - pes vie zit - prida sa mu vek o 1 a
+      tiez sa mu prida hlad o 1
+    (je viac hladny lebo uz presiel rok a nic nejedol)
+    - pes vie jest
+        - vypise do konzoly ze Ham ham
+        - hlad sa mu odpocita o 1,
+            ALE len v pripade ak je hlad vacsi ako 0 !!!
+
+-> vytvorte podla triedy objekt psa s menom Adolf,
+   a ukazte ze vie robit vsetko co ma.
+   Vypisat cely objekt do konzoly mozete jednoducho
+   console.log(pes)
 
 */
