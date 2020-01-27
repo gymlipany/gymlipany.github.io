37dc262fa24547c3e772d161557d385cc03776d3
gymlipany <cervenymed69>
2020-01-27 09:40:58 +0100

    Update index.js

--- andrej/_obejektove_kreslenie_/index.js
+++ andrej/_obejektove_kreslenie_/index.js
@@ -1,9 +1,33 @@
 
+
+class Kruh {
+  constructor(x, y, farba){
+    this.x = x
+    this.y = y
+    this.farba= farba
+  }
+}
+
+let rrs = []
+
 function setup() {
   createCanvas(800, 600)
+  let k1 = new Kruh(12, 34, "red" )
+  let k2 = new Kruh(22, 66, "green")
+  let k3 = new Kruh(11, 12, "blue")
+  rrs.push(k1)
+  
+  rrs.push(k2)
+  
+  rrs.push(k3)
 }
 
 function draw() {
   background(0)
-  
+
+  for (let krKruh of rrs){
+    fill(krKruh.farba)
+    circle(krKruh.x,krKruh.y, 30)
+    
+  }
 }
